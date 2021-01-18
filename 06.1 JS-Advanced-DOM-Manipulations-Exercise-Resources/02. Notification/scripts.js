ac0b0a547df8929c90853115f1b75c50e6b0ceaa
vekr0w <vekr0w@gmail.com>
2021-01-18 13:53:13 +0200

    Notification Completed

--- 06.1 JS-Advanced-DOM-Manipulations-Exercise-Resources/02. Notification/scripts.js
+++ 06.1 JS-Advanced-DOM-Manipulations-Exercise-Resources/02. Notification/scripts.js
@@ -1,3 +1,10 @@
 function notify(message) {
-    console.log('TODO:...');
+  // string 'Something happened!'
+  const notificationElement = document.querySelector("#notification");
+  notificationElement.innerHTML = message;
+  notificationElement.style.display = "block";
+  setTimeout(() => {
+    notificationElement.style.display = "none";
+  }, 2000);
+
 }
