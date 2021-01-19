9f787aef11ba3ccdc54797285225202b84984970
vekr0w <vekr0w@gmail.com>
2021-01-19 15:39:08 +0200

    company exercise, not finised

---
+++ 07-Function-Context/Company.js
@@ -0,0 +1,19 @@
+class Company {
+  constructor() {
+    this.department = [];
+    this.addEmployee = this.addEmployee(name, salary, position, department)  {
+      const isInputValid = (name && salary && position && department) || salary < 0;
+    },
+    this.bestDepartment = bestDepartment() {
+
+    }
+  }
+}
+let proxiad = new Company();
+console.log(proxiad);
+
+
+
+
+proxiad.addEmployee("Stanimir", 2000, "engineer", "Construction");
+console.log(proxiad);
