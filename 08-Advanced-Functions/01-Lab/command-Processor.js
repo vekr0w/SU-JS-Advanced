7445f25ffe38c116e26426e7ac237d60ad01aeaf
vekr0w <vekr0w@gmail.com>
2021-01-28 14:49:41 +0200

    create

---
+++ 08-Advanced-Functions/01-Lab/command-Processor.js
@@ -0,0 +1,25 @@
+/*
+Write a program that keeps a string inside its context and can execute different commands
+that modify or print the string on the console.
+append(string) - append the given string at the end of the internal string
+removeStart(n) - remove the first n characters from the string, n is an integer
+removeEnd(n) - remove the last n characters from the string, n is an integer
+print - print the stored string on the console
+
+*/
+
+function solution() {
+  let string = '';
+
+  return function operators(command) {
+    
+  }
+}
+
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
